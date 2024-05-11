6be7295989c4669ee0563d7cd96ccb98250bd72d
Paul Peavyhouse <pv@swooby.com>
2024-05-10 22:14:28 -0700

    Initial add of function updateHandler

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,62 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-const {onRequest} = require("firebase-functions/v2/https");
+const firestore = require("firebase-admin/firestore");
+const functions = require("firebase-functions");
 const logger = require("firebase-functions/logger");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const axios = require("axios");
+const cors = require("cors");
+const express = require("express");
+const app = express();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const {initializeApp} = require("firebase-admin/app");
+initializeApp();
+
+app.use(cors({origin: true}));
+
+/**
+ * To test:
+ * https://obsndiproject.com/update?obsGuid=xyz&obsndiVersion=1.2.3
+ * https://obs-ndi.web.app/update?obsGuid=xyz&obsndiVersion=1.2.3
+ * http://127.0.0.1:5002/update?obsGuid=xyz&obsndiVersion=1.2.3
+ */
+app.get("/update", async (req, res) => {
+  try {
+    const obsGuid = req.query.obsGuid;
+    const obsndiVersion = req.query.obsndiVersion;
+    if (!obsGuid || !obsndiVersion) {
+      res.sendStatus(403);
+      return;
+    }
+
+    const updateInfo = {obsGuid, obsndiVersion, timestamp: new Date()};
+    logger.info("updateInfo", updateInfo);
+
+    const db = firestore.getFirestore();
+    const doc = db.doc(`updates/${obsGuid}`);
+    await doc.set(updateInfo);
+
+    const response = await axios.get("https://api.github.com/repos/obs-ndi/obs-ndi/releases/latest");
+    const responseData = response.data;
+    // logger.info("responseData", responseData);
+    // parse responseData json for
+    // {
+    //   "html_url": "https://github.com/obs-ndi/obs-ndi/releases/tag/4.13.2",
+    //   "tag_name": "4.13.2",
+    //   "name": "OBS-NDI 4.13.2",
+    //   "created_at": "2024-04-28T17:18:27Z",
+    //   "published_at": "2024-05-02T20:36:30Z",
+    // }
+    const data = {
+      tag_name: responseData.tag_name,
+      name: responseData.name,
+      html_url: responseData.html_url,
+      created_at: responseData.created_at,
+      published_at: responseData.published_at,
+    };
+    logger.info("data", data);
+    res.json(data);
+  } catch (error) {
+    res.status(500).send(error.toString());
+  }
+});
+
+exports.updateHandler = functions.https.onRequest(app);
